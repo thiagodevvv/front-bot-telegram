5f2c5adbe4fbb217b5fbc4b1e5878ead81367bcc
thiago henrique da silva <th.ferrari@outlook.com.br>
2022-04-05 17:28:43 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './style/App.css';
+import SideBar from './SideBar';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1 style={{fontSize: 25, marginLeft: 10}}>Dashboard</h1>
       </header>
+      < SideBar />
     </div>
   );
 }
---
+++ src/SideBar.js
@@ -0,0 +1,12 @@
+import './style/SideBar.css'
+
+function SideBar() {
+  return (
+    <div className="SideBar">
+
+    </div>
+  )
+}
+
+
+export default SideBar
