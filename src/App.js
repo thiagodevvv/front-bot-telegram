04176e5db8c38c4b53ab8fae24ccf222ec30881d
thiago henrique da silva <th.ferrari@outlook.com.br>
2022-04-06 19:39:09 -0300

    add icons in SideBar

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 style={{fontSize: 25, marginLeft: 10}}>Dashboard</h1>
+        <h1 style={{fontSize: 25, marginLeft: 20}}>Dashboard - Bot Atendente Telegram</h1>
       </header>
       < SideBar />
     </div>
--- src/SideBar.js
+++ src/SideBar.js
@@ -3,7 +3,18 @@ import './style/SideBar.css'
 function SideBar() {
   return (
     <div className="SideBar">
-
+      <div onClick={() => {}} className='box-icons-sidebar'>
+        <img alt='Início' className='Icons' src="home.png"  />
+        <p>Inicio</p>
+      </div>
+      <div className='box-icons-sidebar'>
+        <img alt='Enviar Mensagens' className='Icons' src="send.png" />
+        <p>Disparar Mensagens</p>
+      </div>
+      <div className='box-icons-sidebar'>
+        <img alt='Ver Histórico' className='Icons' src="history.png" />
+        <p>Histórico de Pedidos</p>  
+      </div>
     </div>
   )
 }
