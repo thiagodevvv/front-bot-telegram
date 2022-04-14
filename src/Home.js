8838f931c18249ac9c05ba4df53d4e33097fb3ae
thiago henrique da silva <th.ferrari@outlook.com.br>
2022-04-14 14:31:38 -0300

    refat modal in file alone

--- src/Home.js
+++ src/Home.js
@@ -7,7 +7,7 @@ import Dustbin  from './dnd/Dustbin.js'
 import DustbinFinish from './dnd/DustbinFinish'
 import axios from 'axios'
 import { Context } from './Context/StateContext'
-
+import Modal from './ModalCancelar.js'
 // const pedidos = [ {
 //   "_id": "624ad8daf46883dfe21bbee7",
 //   "carrinho": [
@@ -81,44 +81,6 @@ async function pollingPedidos(setPedidos) {
   pollingTimeout = setTimeout(() => pollingPedidos(setPedidos), 1500)
 }
 
-
-async function cancelarPedido (id, controlVisible) {
-  try {
-    const response = await axios.post('http://localhost:8000/dev/alterarStatusPedido', {
-      status: 99,
-      id: id
-    })
-    console.log('cancelando')
-    console.log(response.data)
-    controlVisible()
-    alert('Sucesso ao cancelar')
-  }catch(err) {
-    alert('Error ao cancelar')
-  }
-}
-
-function Modal ({controlVisible, pedido}) {
-  return (
-    <div style={{width: '100%', backgroundColor: 'white'}}>
-      <div className='modal-cancelar'>
-        <h1 style={{textAlign: 'center'}}>Cancelar Pedido</h1>
-        <div>
-          Número Pedido: {pedido._id} <br></br><br></br>
-          {pedido.nomeCliente}
-          {pedido.carrinho.map((produto, index) => {
-            return <p key={index}>{produto.produto} - Qnt: {produto.qnt}</p>
-          })}
-          Total pedido: {parseInt(pedido.totalPedido).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}<br/> <br/>
-          {pedido.endereco === 'retirada123$$' ? 'Retirada' :  `Entrega: ${pedido.endereco}`}
-        </div>
-        <div style={{display: 'flex', 'flexDirection': 'row', marginTop: 30}}>
-          <button className='btn-confirma-cancelar' onClick={() => cancelarPedido(pedido._id, controlVisible)} >Confirmar</button>
-          <button className='btn-nao-cancelar' onClick={() => controlVisible()}>Não confirma</button>
-        </div>
-      </div>
-    </div>
-  )
-}
 
 export default function Home () {
   const { isVisibleModal, setOpenModal, pedidoCancelar } = useContext(Context)
---
+++ src/ModalCancelar.js
@@ -0,0 +1,41 @@
+import axios from 'axios'
+
+async function cancelarPedido (id, controlVisible) {
+  try {
+    const response = await axios.post('http://localhost:8000/dev/alterarStatusPedido', {
+      status: 99,
+      id: id
+    })
+    console.log('cancelando')
+    console.log(response.data)
+    controlVisible()
+    alert('Sucesso ao cancelar')
+  }catch(err) {
+    alert('Error ao cancelar')
+  }
+}
+
+
+
+export default function Modal ({controlVisible, pedido}) {
+  return (
+    <div style={{width: '100%', backgroundColor: 'white'}}>
+      <div className='modal-cancelar'>
+        <h1 style={{textAlign: 'center'}}>Cancelar Pedido</h1>
+        <div>
+          Número Pedido: {pedido._id} <br></br><br></br>
+          {pedido.nomeCliente}
+          {pedido.carrinho.map((produto, index) => {
+            return <p key={index}>{produto.produto} - Qnt: {produto.qnt}</p>
+          })}
+          Total pedido: {parseInt(pedido.totalPedido).toLocaleString('pt-br',{style: 'currency', currency: 'BRL'})}<br/> <br/>
+          {pedido.endereco === 'retirada123$$' ? 'Retirada' :  `Entrega: ${pedido.endereco}`}
+        </div>
+        <div style={{display: 'flex', 'flexDirection': 'row', marginTop: 30}}>
+          <button className='btn-confirma-cancelar' onClick={() => cancelarPedido(pedido._id, controlVisible)} >Confirmar</button>
+          <button className='btn-nao-cancelar' onClick={() => controlVisible()}>Não confirma</button>
+        </div>
+      </div>
+    </div>
+  )
+}
